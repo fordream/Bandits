88c459d15d9769c0e5f100f6c9598367e589b025
Yuuichiro KATO <yonesuke777@gmail.com>
2015-05-07 20:19:58 +0900

    FactoryGirlのnamespaceをインクルード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
 
   it "名前があれば有効" do
-    expect(FactoryGirl.build(:user, name: 'foo')).to be_valid
+    expect(build(:user, name: 'foo')).to be_valid
   end
 
   it "名前がなければ無効" do
-    expect(FactoryGirl.build(:user, name: nil)).not_to be_valid
+    expect(build(:user, name: nil)).not_to be_valid
   end
 
 end
