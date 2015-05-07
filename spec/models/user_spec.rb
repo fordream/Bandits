389c9527f55591326444a498c76a0798bbb1dceb
Yuuichiro KATO <yonesuke777@gmail.com>
2015-05-07 19:45:35 +0900

    User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+
+  it "名前があれば有効" do
+    expect(FactoryGirl.build(:user, name: 'foo')).to be_valid
+  end
+
+  it "名前がなければ無効" do
+    expect(FactoryGirl.build(:user, name: nil)).not_to be_valid
+  end
+
+end
